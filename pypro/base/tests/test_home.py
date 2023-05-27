97b64247e7e4552410834fd473cf0bf6ccad568d
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-05-26 21:31:56 -0300

    enviando flake8

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,5 +1,6 @@
 from django.test import Client
 
-def test_status_code(client:Client):
-    resp = client.get('/') # está pegando o status da pagina home
+
+def test_status_code(client: Client):
+    resp = client.get('/')  # está pegando o status da pagina home
     assert resp.status_code == 200
