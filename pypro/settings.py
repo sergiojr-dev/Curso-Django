b7a673394808e31a703026e2ef14dc15fda09021
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-06-03 14:06:56 -0300

    enviando configurações do ALLOWED_HOST

--- pypro/settings.py
+++ pypro/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.fly.dev']
+
 
 AUTH_USER_MODEL = 'base.User'
 
