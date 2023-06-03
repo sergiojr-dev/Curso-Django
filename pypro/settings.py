b4403fc97d4e1895549f83be909396dab7478bf9
sergio0006 <sergio.ana@aluno.faculdadeimpacta.com.br>
2023-06-03 14:25:30 -0300

    enviando nova configuração de allowed_hosts

--- pypro/settings.py
+++ pypro/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.fly.dev']
 
 AUTH_USER_MODEL = 'base.User'
 
